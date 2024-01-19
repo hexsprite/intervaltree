437e893f4e9736f87635f36bc4f11e3aff05260c
Jordan Baker <jbb@scryent.com>
2024-01-19 12:06:30 -0700

    Update launch.json and package.json

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { Interval } from './Interval'
-import { IntervalTree, SimpleIntervalArray } from './IntervalTree'
-import { enableDebug } from './debug'
-
-export { enableDebug, Interval, IntervalTree, SimpleIntervalArray }
